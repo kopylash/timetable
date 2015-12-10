16f64bc51be95157ac6d1cdce823daeb31753274
Vladyslav Kopylash <vkop@grdnz.com>
2015-12-10 02:02:43 +0200

    added buttons for changeWeek

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Link, IndexLink } from 'react-router'
-import { RaisedButton }  from 'material-ui'
 
-const ACTIVE = {color: 'red'};
 const Tabs = require('material-ui/lib/tabs/tabs');
 const Tab = require('material-ui/lib/tabs/tab');
 const List = require('material-ui/lib/lists/list');
 const ListItem = require('material-ui/lib/lists/list-item');
-const SvgIcon = require('material-ui/lib/svg-icon');
 const ListDivider = require('material-ui/lib/lists/list-divider');
+const IconMenu = require('material-ui/lib/menus/icon-menu');
+const MenuItem = require('material-ui/lib/menus/menu-item');
+const IconButton = require('material-ui/lib/icon-button');
+const NavigationMoreVert = require('material-ui/lib/svg-icons/navigation/more-vert');
+const AppBar = require('material-ui/lib/app-bar');
 
 let injectTapEventPlugin = require("react-tap-event-plugin");
 //Needed for onTouchTap
@@ -21,120 +22,230 @@ injectTapEventPlugin();
 class App extends React.Component {
   constructor() {
     super();
+    this.changeWeek = this.changeWeek.bind(this);
     this.state = {
-      days: [
-        {
-          name: 'ПН',
-          lessons: [
+      currentWeek: 'firstWeek',
+      weeks: {
+        firstWeek: {
+          days: [
             {
-              name: 'Блок1. АСНД',
-              lecturer: 'Польшакова О.М',
-              room: '18-418 лекція',
-              time: '10:25 - 12:00'
+              name: 'ПН',
+              lessons: [
+                {
+                  name: 'Блок1. АСНД',
+                  lecturer: 'Польшакова О.М',
+                  room: '18-418 лекція',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Іноземна мова професійного спрямування - 2',
+                  lecturer: 'Колісник М.П.',
+                  room: '18-411',
+                  time: '12:20 - 13:55'
+                }
+              ]
             },
             {
-              name: 'Іноземна мова професійного спрямування - 2',
-              lecturer: 'Колісник М.П.',
-              room: '18-411',
-              time: '12:20 - 13:55'
+              name: 'ВТ',
+              lessons: [
+                {
+                  name: 'Блок1. Тестування та забезпечення кості програмних систем',
+                  lecturer: 'Максимюк А.В.',
+                  room: '18-415 лабораторне заняття',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Технології розробки програмного забезпечення - 4. Управління проектами',
+                  lecturer: 'Савицький А.Й.',
+                  room: '18-418 лекція',
+                  time: '12:20 - 13:55'
+                }
+              ]
+            },
+            {
+              name: 'СР',
+              lessons: [
+                {
+                  name: 'CAD/CAM/CAE системи',
+                  lecturer: 'Поліщук М.М.',
+                  room: '18-419 лекція',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Захист інформації в комп\'ютерних мережах',
+                  lecturer: 'Пасько В.П.',
+                  room: '18-419 лекція',
+                  time: '12:20 - 13:55'
+                }
+              ]
+            },
+            {
+              name: 'ЧТ',
+              lessons: [
+                {
+                  name: 'Блок1. Тестування та забезпечення кості програмних систем',
+                  lecturer: 'Савицький А.Й.',
+                  room: '18-418 лекція',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Іноземна мова професійного спрямування - 2',
+                  lecturer: 'Колісник М.П.',
+                  room: '18-411',
+                  time: '12:20 - 13:55'
+                }
+              ]
+            },
+            {
+              name: 'ПТ',
+              lessons: [
+                {
+                  name: 'Корпоративні інформаційні системи та технології',
+                  lecturer: 'Тимошин Ю.А.',
+                  room: '18-418а лабораторне заняття',
+                  time: '8:30 - 10:05'
+                },
+                {
+                  name: 'Корпоративні інформаційні системи та технології',
+                  lecturer: 'Тимошин Ю.А.',
+                  room: '18-432 лекція',
+                  time: '8:30 - 10:05'
+                },
+                {
+                  name: 'Захист інформації в комп\'ютерних мережах',
+                  lecturer: 'Пасько В.П.',
+                  room: '18-419 лекція',
+                  time: '12:20 - 13:55'
+                }
+              ]
             }
           ]
         },
-        {
-          name: 'ВТ',
-          lessons: [
+        secondWeek: {
+          days: [
             {
-              name: 'Блок1. Тестування та забезпечення кості програмних систем',
-              lecturer: 'Максимюк А.В.',
-              room: '18-415 лабораторне заняття',
-              time: '10:25 - 12:00'
+              name: 'ПН',
+              lessons: [
+                {
+                  name: 'Блок1. АСНД',
+                  lecturer: 'Польшакова О.М',
+                  room: '18-418 лекція',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Іноземна мова професійного спрямування - 2',
+                  lecturer: 'Колісник М.П.',
+                  room: '18-411',
+                  time: '12:20 - 13:55'
+                }
+              ]
             },
             {
-              name: 'Технології розробки програмного забезпечення - 4. Управління проектами',
-              lecturer: 'Савицький А.Й.',
-              room: '18-418 лекція',
-              time: '12:20 - 13:55'
-            }
-          ]
-        },
-        {
-          name: 'СР',
-          lessons: [
-            {
-              name: 'CAD/CAM/CAE системи',
-              lecturer: 'Поліщук М.М.',
-              room: '18-419 лекція',
-              time: '10:25 - 12:00'
+              name: 'ВТ',
+              lessons: [
+                {
+                  name: 'Блок1. Тестування та забезпечення кості програмних систем',
+                  lecturer: 'Максимюк А.В.',
+                  room: '18-415 лабораторне заняття',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Технології розробки програмного забезпечення - 4. Управління проектами',
+                  lecturer: 'Савицький А.Й.',
+                  room: '18-418 лекція',
+                  time: '12:20 - 13:55'
+                }
+              ]
             },
             {
-              name: 'Захист інформації в комп\'ютерних мережах',
-              lecturer: 'Пасько В.П.',
-              room: '18-419 лекція',
-              time: '12:20 - 13:55'
-            }
-          ]
-        },
-        {
-          name: 'ЧТ',
-          lessons: [
-            {
-              name: 'Блок1. Тестування та забезпечення кості програмних систем',
-              lecturer: 'Савицький А.Й.',
-              room: '18-418 лекція',
-              time: '10:25 - 12:00'
+              name: 'СР',
+              lessons: [
+                {
+                  name: 'CAD/CAM/CAE системи',
+                  lecturer: 'Поліщук М.М.',
+                  room: '18-419 лекція',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Захист інформації в комп\'ютерних мережах',
+                  lecturer: 'Пасько В.П.',
+                  room: '18-419 лекція',
+                  time: '12:20 - 13:55'
+                }
+              ]
             },
             {
-              name: 'Іноземна мова професійного спрямування - 2',
-              lecturer: 'Колісник М.П.',
-              room: '18-411',
-              time: '12:20 - 13:55'
-            }
-          ]
-        },
-        {
-          name: 'ПТ',
-          lessons: [
-            {
-              name: 'Корпоративні інформаційні системи та технології',
-              lecturer: 'Тимошин Ю.А.',
-              room: '18-418а лабораторне заняття',
-              time: '8:30 - 10:05'
+              name: 'ЧТ',
+              lessons: [
+                {
+                  name: 'Блок1. Тестування та забезпечення кості програмних систем',
+                  lecturer: 'Савицький А.Й.',
+                  room: '18-418 лекція',
+                  time: '10:25 - 12:00'
+                },
+                {
+                  name: 'Іноземна мова професійного спрямування - 2',
+                  lecturer: 'Колісник М.П.',
+                  room: '18-411',
+                  time: '12:20 - 13:55'
+                }
+              ]
             },
             {
-              name: 'Корпоративні інформаційні системи та технології',
-              lecturer: 'Тимошин Ю.А.',
-              room: '18-432 лекція',
-              time: '8:30 - 10:05'
-            },
-            {
-              name: 'Захист інформації в комп\'ютерних мережах',
-              lecturer: 'Пасько В.П.',
-              room: '18-419 лекція',
-              time: '12:20 - 13:55'
+              name: 'ПТ',
+              lessons: [
+                {
+                  name: 'Корпоративні інформаційні системи та технології',
+                  lecturer: 'Тимошин Ю.А.',
+                  room: '18-418а лабораторне заняття',
+                  time: '8:30 - 10:05'
+                },
+                {
+                  name: 'Корпоративні інформаційні системи та технології',
+                  lecturer: 'Тимошин Ю.А.',
+                  room: '18-432 лекція',
+                  time: '8:30 - 10:05'
+                },
+                {
+                  name: 'Захист інформації в комп\'ютерних мережах',
+                  lecturer: 'Пасько В.П.',
+                  room: '18-419 лекція',
+                  time: '12:20 - 13:55'
+                }
+              ]
             }
           ]
         }
-      ]
-
+      }
     }
+
   }
+
+  changeWeek(event, value) {
+    console.log(value);
+    this.state.currentWeek = value;
+  }
+
 
   render() {
     return (
       <div>
-        <div style={{
-          width: '100%',
-          lineHeight: '40px',
-          background: '#00bcd4',
-          color: '#fff',
-          fontWeight: '500',
-          textAlign: 'center',
-          textTransform: 'uppercase'
-        }}>
-          Campus timetable
-        </div>
+        <AppBar
+          title="Campus timetable"
+          style={{boxShadow: ' 0 1px 0px rgba(0, 0, 0, 0.24)'}}
+          showMenuIconButton={false}
+          iconElementRight={
+            <IconMenu
+              value={this.state.currentWeek}
+              iconButtonElement={<IconButton><NavigationMoreVert /></IconButton>}
+              onChange={this.changeWeek}
+            >
+              <MenuItem primaryText="1 тиждень" value="firstWeek"/>
+              <MenuItem primaryText="2 тиждень" value="secondWeek" />
+            </IconMenu>
+            } />
         <Tabs>
-        {this.state.days.map(function(day, index) {
+        {this.state.weeks[this.state.currentWeek].days.map(function(day, index) {
           return (
             <Tab label={day.name} key={index} >
               <List>
@@ -147,11 +258,11 @@ class App extends React.Component {
                       secondaryText={<LessonInfo room={item.room} lecturer={item.lecturer} time={item.time} />}
                       leftIcon={<i style={{
                         display: 'block',
-                        top:'27%',
-                        left:'12px',
-                        padding:'0',
+                        top: '27%',
+                        left: '12px',
+                        padding: '0',
                         background: 'url(img/number' + number + '.svg) no-repeat #FFA5C4',
-                        borderRadius:'36px',
+                        borderRadius: '36px',
                         backgroundSize: '48px 48px',
                         width: '48px',
                         height: '48px'
@@ -207,11 +318,4 @@ LessonInfo.defaultProps = {
   time: ''
 };
 
-render((
-  <Router >
-    <Route path="/" component={App}>
-
-
-    </Route>
-  </Router>
-), document.getElementById('content'));
+render(<App />, document.getElementById('content'));
